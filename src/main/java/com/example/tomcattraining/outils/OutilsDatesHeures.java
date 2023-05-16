c6eabe89ad17f43cf985d85fbb0165edec41219f
TakyL <takinou57@gmail.com>
2023-05-16 21:21:54 +0200

    fix date format bug due to convert

--- src/main/java/com/example/tomcattraining/outils/OutilsDatesHeures.java
+++ src/main/java/com/example/tomcattraining/outils/OutilsDatesHeures.java
@@ -3,40 +3,66 @@ package com.example.tomcattraining.outils;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 public class OutilsDatesHeures {
 
 
     /**
-     * Vérifie si l'input est bien au format spécifié
+     * Vérifie si une date est bien au format dd/mm/yyyy
      * @return
      */
-    static public boolean isDateFormatted(Date dateformat)
+    static public boolean isFormatddMMyyyy(String formatdate)
     {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        formatter.setLenient(false); // disable lenient parsing
-        String dateString = formatter.format(dateformat);
-        return dateString.equals(formatter.format(dateString));
+        String regex = "^\\d{2}/\\d{2}/\\d{4}$";
+        return Pattern.matches(regex, formatdate);
     }
 
     /**
-     * Formatte un string date au format dd/mm/yyyy
-     * @param input
-     * @return
+     * Formatte un string date au format dd/mm/yyyy si besoin
+     * @param input : String au format yyyy/mm/dd
+     * @return Date au format dd/mm/yyyy
      */
-    static public Date formateDate(String input)
+    static public String formateDate(String input)
     {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        try {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        dateFormat.setLenient(false);
 
-           return formatter.parse(input);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        return FormateDateYYYYMMDDtoDDMMYYYY(input);
 
-        throw new RuntimeException("Le formattage de la date n'a pas réussi ! ");
     }
 
+    /**
+     * Formatte une date dd/mm/yyyy , conversion au format yyyy/mm/dd au format dd/mm/yyyy si besoin
+     * @param input : Date au format yyyy/mm/dd
+     * @return Date au format dd/mm/yyyy
+     * @throws RuntimeException si le format saisi ne correspond ni à dd/mm/yyyy
+     */
+    static private String FormateDateYYYYMMDDtoDDMMYYYY(String input) {
+        SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            if(isFormatYYYYMMDD(input))
+            {
+                Date date = inputDateFormat.parse(input);
+                SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                return outputDateFormat.format(date);
+            }
+            else if(isFormatddMMyyyy(input))//La date est est déjà au bon format
+            {
+                return input;
+            }
+
+            throw new RuntimeException("Le système n'a pas réussi à convertir la date ");
+        } catch (ParseException e) {
+            throw new RuntimeException("Erreur lors de la conversion en yyyy" + input);
+        }
+    }
+
+    static private boolean isFormatYYYYMMDD(String string)
+    {
+        String regex = "^\\d{4}/\\d{2}/\\d{2}$";
+        return Pattern.matches(regex, string);
+    }
     /**
      * Vérifie le format de l'heure
      * @param input
--- src/main/java/outilshibernate/QueryRdv.java
+++ src/main/java/outilshibernate/QueryRdv.java
@@ -5,21 +5,74 @@ import org.hibernate.Session;
 
 import java.util.List;
 
+import static com.example.tomcattraining.outils.OutilsString.verifieSiChainesEquivalentes;
+
 public class QueryRdv implements Query{
     @Override
-    public List findAllElmt() {
+    public List<RendezVous> findAllElmt() {
         Session session1 = OutilsHibernate.getSession();
         session1.beginTransaction();
 
         // fetch all RendezVous objects from the database
         List<RendezVous> rendezVousList = session1.createQuery("FROM RendezVous", RendezVous.class).list();
 
-        // iterate over the list of RendezVous objects and add them to the table model
-        for (RendezVous rendezVous : rendezVousList) {
-            Object[] rowData = { rendezVous.getDate_rdv(), rendezVous.getHeure_rdv(), rendezVous.getNom_client(), rendezVous.getNom_employe() };
-        }
-
         session1.getTransaction().commit();
         return rendezVousList;
+    }
+
+    @Override
+    public boolean insertItem(Object item) {
+        if(!isAlreadyExisting((RendezVous) item) && !hasAlreadyaRdv((RendezVous) item))
+        {
+            System.out.println("OK");
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Vérifie si un rendez vous existe déjà dans la base de données
+     * @param rdv
+     * @return
+     */
+
+    private boolean isAlreadyExisting(RendezVous rdv) {
+
+        List<RendezVous> list =findAllElmt();
+        for (RendezVous r:list) {
+            if(r.getDate_rdv().equals(rdv.getDate_rdv()))//Si date identique
+            {
+                if(verifieSiChainesEquivalentes(r.getHeure_rdv(),rdv.getHeure_rdv()) && verifieSiChainesEquivalentes(r.getNom_employe(),rdv.getNom_employe()) && verifieSiChainesEquivalentes(rdv.getNom_client(),r.getNom_client()))
+                {
+                    System.out.println("Ce rendez vous existe déjà");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Recherche si le client/employé possède déjà un rendez vous pour cette tranche horaire
+     * @param rdv : le rendez vous que l'on veut ajouter
+     * @return true si client/employé possède déjà un rendez vous pour cette tranche horaire
+     */
+    private boolean hasAlreadyaRdv(RendezVous rdv) {
+
+        List<RendezVous> list =findAllElmt();
+        for (RendezVous r:list) {
+            if(r.getDate_rdv().equals(rdv.getDate_rdv()) && verifieSiChainesEquivalentes(r.getHeure_rdv(), rdv.getHeure_rdv()))//Si date identique et heure identique
+            {
+
+                //Si nom est égale ou bien de le nom de l'employé a été trouvé alors il a déjà un rdv pour cette tranche horaire
+                if(verifieSiChainesEquivalentes(rdv.getNom_employe(),r.getNom_employe()) || verifieSiChainesEquivalentes(rdv.getNom_client(),r.getNom_client()))//Si le nom d'un employé est egale à ce nom alors
+                {
+                    System.out.println("L'un de deux utilisateurs possède déjà un rdv");
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
